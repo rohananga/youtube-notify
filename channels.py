5e7963ffe7899bd0b41a5102aba040a750217e91
Amar Mujumdar <65742017+amarmuju@users.noreply.github.com>
2020-07-08 23:15:24 -0400

    modified algorithm to include search function (since username may differ from channel name)

--- channels.py
+++ channels.py
@@ -5,9 +5,10 @@ api_key = "AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8"
 youtube = build('youtube', 'v3', developerKey = api_key)
 
 # first retrieve channel ID of "username"
-username = "CaptainSparklez"
-channel = youtube.channels().list(part = 'id', forUsername = username).execute()
-currentId = channel['items'][0]['id']
+username = "Netflix"
+index = 0 # really up to user on which channel to pick
+channel = youtube.search().list(part = 'snippet', maxResults = 5, q = username, type = 'channel').execute()
+currentId = channel['items'][index]['id']['channelId']
 
 # use channel ID to get most recent video
 res = youtube.search().list(part='snippet', channelId = currentId, maxResults = '5', order = 'date',
