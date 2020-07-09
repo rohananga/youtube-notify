20235e727d5089e5524f1abc75d78353cd9c9877
Amar Mujumdar <65742017+amarmuju@users.noreply.github.com>
2020-07-08 20:39:19 -0400

    added comments and added query for getting channel ID first

--- channels.py
+++ channels.py
@@ -1,17 +1,18 @@
-api_key = "AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8"
-
-
+# setup, imports, and API key
 from apiclient.discovery import build
 
+api_key = "AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8"
 youtube = build('youtube', 'v3', developerKey = api_key)
 
-currentId = 'UCshoKvlZGZ20rVgazZp5vnQ'
+# first retrieve channel ID of "username"
+username = "CaptainSparklez"
+channel = youtube.channels().list(part = 'id', forUsername = username).execute()
+currentId = channel['items'][0]['id']
 
+# use channel ID to get most recent video
 res = youtube.search().list(part='snippet', channelId = currentId, maxResults = '5', order = 'date',
 type = 'video').execute()
-
 result = res['items'][0]['snippet']['title']
-
 print(result)
 
 
