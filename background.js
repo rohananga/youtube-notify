4cee78424bbebe59d4d6920c583bb513f3442048
Amar Mujumdar <65742017+amarmuju@users.noreply.github.com>
2020-07-16 18:02:26 -0400

    encrypted email info using secure token

--- background.js
+++ background.js
@@ -1,17 +1,36 @@
 //background.js
 
+function getVideo() {
+	$.ajax({
+	  type: 'GET',
+	  url: 'https://www.googleapis.com/youtube/v3/search',
+	  data: {
+		  key: 'AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8',
+		  q: "cats",
+		  part: 'snippet',
+		  maxResults: 1,
+		  type: 'video',
+		  videoEmbeddable: true,
+	  },
+	  success: function(data){
+		  alert("Success!");
+	  },
+	  error: function(response){
+		  console.log("Request Failed");
+	  }
+	});
+  }
+
 function sendEmail() {
 	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "amarmuju@terpmail.umd.edu",
-	Password : "",
-	To : 'hummerrocket@gmail.com',
-	From : "amarmuju@terpmail.umd.edu",
-	Subject : "<email subject>",
-	Body : "<email body>",
+	SecureToken : "c12b00bb-a381-4628-97ab-07032d1c145e",
+	To : 'rohankms@gmail.com',
+	From : "myvideocentral2020@gmail.com",
+	Subject : "Test Email",
+	Body : "hey there this is a test",
 	}).then(
 		message => alert("mail sent successfully")
 	);
 }
 
-setTimeout(function(){ sendEmail();}, 10000);
+sendEmail();
