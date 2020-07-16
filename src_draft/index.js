4ab4c8f96ae20849223126534538037859238764
Amar Mujumdar <65742017+amarmuju@users.noreply.github.com>
2020-07-16 16:42:29 -0400

    organized folders and started source code

---
+++ src_draft/index.js
@@ -0,0 +1,15 @@
+//index.js  
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "<sender’s email address>",
+	Password : "<email password>",
+	To : '<recipient’s email address>',
+	From : "<sender’s email address>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
