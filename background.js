168ccea936b79fa65d93aade50033f3eaf721778
Amar Mujumdar <65742017+amarmuju@users.noreply.github.com>
2020-07-21 22:25:44 -0400

    fixed parsing of ajax requests - now works

--- background.js
+++ background.js
@@ -15,25 +15,7 @@ chrome.runtime.onInstalled.addListener(function() {
 	  });
   });
 
-function getChannel(username) {
-	$.ajax({
-	  type: 'GET',
-	  url: 'https://www.googleapis.com/youtube/v3/search',
-	  data: {
-		  key: 'AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8',
-		  q: username,
-		  part: 'snippet',
-		  maxResults: 5,
-		  type: 'channel',
-	  },
-	  success: function(data){
-		  alert("Success!");
-	  },
-	  error: function(response){
-		  console.log("Request Failed");
-	  }
-	});
-  }
+
 
 function sendEmail() {
 	Email.send({
---
+++ homepage.js
@@ -0,0 +1,21 @@
+function getChannel(username) {
+	$.ajax({
+	  type: 'GET',
+	  url: 'https://www.googleapis.com/youtube/v3/search',
+	  data: {
+		  key: 'AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8',
+		  q: username,
+		  part: 'snippet',
+		  maxResults: 5,
+		  type: 'channel',
+	  },
+	  success: function(channel){
+		  alert(channel.items[0].snippet.title);
+	  },
+	  error: function(response){
+		  console.log("Request Failed");
+	  }
+	});
+  }
+
+  getChannel("Netflix");
