6b28bc0c39d6a52c628fe5b226a47df5857a6554
Amar Mujumdar <65742017+amarmuju@users.noreply.github.com>
2020-07-22 14:31:35 -0400

    accounted for asynchronous HTTP requests

--- background.js
+++ background.js
@@ -7,7 +7,6 @@ chrome.runtime.onInstalled.addListener(function() {
 		new chrome.declarativeContent.ShowPageAction();
 		chrome.declarativeContent.onPageChanged.addRules([{
 		  conditions: [new chrome.declarativeContent.PageStateMatcher({
-			
 		  })
 		  ],
 			  actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -15,16 +14,43 @@ chrome.runtime.onInstalled.addListener(function() {
 	  });
   });
 
+function getLatestVideoTimes(channelID) {
+	$.ajax({
+		type: 'GET',
+		url: 'https://www.googleapis.com/youtube/v3/search',
+		data: {
+			key: 'AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8',
+			channelId: channelID,
+			part: 'snippet',
+			maxResults: 5,
+			type: 'video',
+			order: 'date',
+		},
+		success: function(data){
+			alert(decodeHTMLEntities(data.items[0].snippet.title));
+		},
+		error: function(response){
+			console.log("Request Failed");
+		}
+	  });
+}
 
-
-function sendEmail() {
+function sendEmail(body) {
 	Email.send({
 	SecureToken : "c12b00bb-a381-4628-97ab-07032d1c145e",
-	To : 'rohankms@gmail.com',
+	To : '',
 	From : "myvideocentral2020@gmail.com",
 	Subject : "Test Email",
-	Body : "hey there this is a test",
+	Body : body,
 	}).then(
 		message => alert("mail sent successfully")
 	);
 }
+
+function decodeHTMLEntities(text) {
+	var textArea = document.createElement('textarea');
+	textArea.innerHTML = text;
+	return textArea.value;
+  }
+
+getLatestVideoTimes("UCshoKvlZGZ20rVgazZp5vnQ");
--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-function getChannel(username) {
-	$.ajax({
-	  type: 'GET',
-	  url: 'https://www.googleapis.com/youtube/v3/search',
-	  data: {
-		  key: 'AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8',
-		  q: username,
-		  part: 'snippet',
-		  maxResults: 5,
-		  type: 'channel',
-	  },
-	  success: function(channel){
-		  alert(channel.items[0].snippet.title);
-	  },
-	  error: function(response){
-		  console.log("Request Failed");
-	  }
-	});
+async function getChannel(username) {
+	let result;
+
+	try {
+		result = await $.ajax({
+			type: 'GET',
+			url: 'https://www.googleapis.com/youtube/v3/search',
+			data: {
+				key: 'AIzaSyAHUITXF9oo2Ed24HO1IrDRtYqNLv15_b8',
+				q: username,
+				part: 'snippet',
+				maxResults: 5,
+				type: 'channel',
+				datatype: 'json',
+			}
+		});
+		
+		return result;
+	} catch (error) {
+		console.log("Request failed.");
+	}
   }
 
-  //getChannel("Netflix");
+  var x = 3;
+  (async () => {
+	const result = await getChannel("Netflix");
+	//alert(result.items[1].snippet.title);
+	
+})()
